bffe3490e3909e70b3d65ca5c41e77b4b298ee63
ZelendoRen <zelendoren@gmail.com>
2021-02-25 19:04:07 +0200

    first commit

---
+++ solution.js
@@ -0,0 +1,16 @@
+const solution = function (x, y) {
+    if (x != 0 && y != 0 && y <= 100000 && x <= 100000) {
+        let prevSum = y;
+        let number = 0;
+        for (let i = 0; i < x;) {
+            prevSum = prevSum + number;
+            if (number >= (y - 1)) {
+                i++;
+            }
+            number++
+        }
+        let stringifyRes = String(prevSum);
+        return ('Capitan Pron Gold Treasure ID: ' + stringifyRes)
+    }
+    else ('Limits:  Each value of x and y must be at least 1 and no greater than 100,000. Please retry')
+}
